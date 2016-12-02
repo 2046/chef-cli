0dac67b3113a47ccac4539ab5035125babe23eb7
2046 <g@ggg.gd>
2016-12-02 16:09:13 +0800

    add install

--- src/utils/tree.js
+++ src/utils/tree.js
@@ -1,13 +1,12 @@
 import fs from 'co-fs'
 import { join } from 'path'
-import treeify from 'treeify'
 
 export default function *tree(path) {
     let config = require(`${path}/package.json`)
 
     return [
         `${config.name}@${config.version} ${path}`,
-        treeify.asTree(yield treeObj(path)),
+        treeify(yield treeObj(path)),
     ]
 }
 
@@ -20,9 +19,51 @@ function *treeObj(path) {
         if((yield fs.lstat(tmp)).isDirectory()) {
             result[item] = yield treeObj(tmp)
         }else {
-            result[item] = {}
+            result[item] = ''
         }
     }
 
     return result
+}
+
+function treeify(obj) {
+    let tree = ''
+
+    growBranch('.', obj, false, [], (line) => {
+        tree += `${line}\n`
+    })
+
+    return tree
+}
+
+function growBranch(key, root, last, lastStates, callback) {
+    let line, lastStatesCopy
+
+    line = ''
+    lastStatesCopy = lastStates.slice(0)
+
+    if(lastStatesCopy.push([root, last]) && lastStates.length > 0) {
+        lastStates.forEach((lastState, index) => {
+            if (index > 0) {
+                line += (lastState[1] ? ' ' : '│') + ' '
+            }
+        })
+
+        line += prefix(key, last, Object.keys(root).length !== 0) + key
+        callback(line)
+    }
+
+    if(typeof root === 'object') {
+        keys(root).forEach((item, index, array) => {
+            growBranch(item, root[item], ++index === array.length, lastStatesCopy, callback)
+        })
+    }
+}
+
+function prefix(key, last, isDirectory) {
+    return (isDirectory ? (last ? '└─┬' : '├─┬') : (last ? '└──' : '├──')) + ' '
+}
+
+function keys(obj) {
+    return Object.keys(obj).filter((key) => obj.hasOwnProperty(key))
 }
